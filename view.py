90b9346141db96d00e8e5aea1acc457b792c1e2d
Rattapoom Pethputong <celica2548@gmail.com>
2024-05-01 17:08:18 +0700

    refractor variables

--- view.py
+++ view.py
@@ -14,19 +14,18 @@ class TabManager(tk.Tk):
     def __init__(self) -> None:
         super().__init__()
         self.title('UK Flight')
-        self.graphs = {}
+        self.graph_factory = GraphFactory
+        self.graph_dict = {'Compare Delay with Previous Year':'Corr'}
         self.init_components()
 
     def init_components(self):
-        tab_controller = ttk.Notebook(self)
+        self.tab_controller = ttk.Notebook(self)
         pack = {'side':'left', 'expand':True, 'fill':'both'}
-        graph_dict = {'Compare Delay with Previous Year': CorrGraph}
-        for key, val in graph_dict.items():
-            graph = val(self)
-            self.graphs[key] = graph
+        for key,val in self.graph_dict.items():
+            graph = self.graph_factory.get_instance(val, self)
             graph.pack(pack)
-            tab_controller.add(graph, text=key)
-        tab_controller.pack(expand=True, fill='both')
+            self.tab_controller.add(graph, text=key)
+        self.tab_controller.pack(expand=True, fill='both')
 
     def exit(self):
         plt.close('all')
@@ -35,9 +34,32 @@ class TabManager(tk.Tk):
         self.protocol('WM_DELETE_WINDOW', exit)
         self.mainloop()
 
-class Graph(tk.Frame, abc.ABC):
+    def get_all_graphs(self):
+        return self.graph_factory.instances.values()
+
+    def get_current_tab(self):
+        return self.tab_controller.tab(self.tab_controller.select(), "text")
+
+class GraphFactory(tk.Frame, abc.ABC):
+
+    instances = {}
+
+    @classmethod
+    def get_instance(cls, name, master=None):
+        """Gets graph instance"""
+        if name not in cls.instances:
+            factories = {'Corr': CorrGraph}
+            if name not in factories:
+                raise ValueError('Invalid graph')
+            if master is None:
+                raise ValueError('Master Needed to initialise graph')
+            cls.instances[name] = factories[name](master)
+        return cls.instances[name]
+
     def __init__(self, master=None, cnf={}, **kwargs):
         super().__init__(master, cnf, **kwargs)
+        self.__description = tk.StringVar()
+        self.__description.set('hello')
         self.init_components()
 
     def init_components(self):
@@ -45,10 +67,15 @@ class Graph(tk.Frame, abc.ABC):
         self.side_panel.pack(side='right', padx=10)
         self.add_side_panel_elements()
 
+        graph_frame = tk.Frame(self)
+        label = tk.Label(graph_frame, textvariable=self.__description)
+        label.pack(anchor=tk.W)
+
         self.fig = plt.figure()
-        self.canvas = FigureCanvasTkAgg(figure=self.fig, master=self)
-        NavigationToolbar2Tk(self.canvas, self)
+        self.canvas = FigureCanvasTkAgg(figure=self.fig, master=graph_frame)
+        NavigationToolbar2Tk(self.canvas, graph_frame)
         self.canvas.get_tk_widget().pack(expand=True, fill='both')
+        graph_frame.pack(expand=True, fill='both')
 
     @abc.abstractmethod
     def add_side_panel_elements(self):
@@ -58,17 +85,13 @@ class Graph(tk.Frame, abc.ABC):
     def plot_graph(self, caller):
         raise NotImplementedError('Abstract Method')
 
-    @property
-    @abc.abstractmethod
-    def get_first_combobox(self):
-        raise NotImplementedError('Abstract Property')
 
-class CorrGraph(Graph):
+class CorrGraph(GraphFactory):
     def __init__(self, master=None, cnf={}, **kwargs):
         super().__init__(master, cnf, **kwargs)
 
     def add_side_panel_elements(self):
-        self.side_panel.create_selector('Airport')
+        self.side_panel.create_selector('Airline')
         self.side_panel.create_selector('Origin (Optional)')
         self.side_panel.create_selector('Destination (Optional)')
         self.side_panel.create_button('PLOT')
@@ -86,10 +109,6 @@ class CorrGraph(Graph):
         sns.barplot(data=data)
         self.canvas.draw()
 
-    @property
-    def get_first_combobox(self):
-        return 'Airport'
-
 class SidePanel(tk.Frame):
     def __init__(self, master=None,cnf={},**kwargs):
         super().__init__(master, cnf, **kwargs)
@@ -101,28 +120,44 @@ class SidePanel(tk.Frame):
         selector = Selector(self)
         selector.label = name
         self.selectors[name] = selector
-        selector.set_state('disabled')
+        selector.set_state('readonly')
         selector.pack(self.padding)
 
     def get_selector(self, name):
+        """Get selector by name"""
         if name in self.selectors:
             return self.selectors[name]
         return None
 
-    def set_selector_value(self, name, val):
-        self.selectors[name].combobox_val = val
+    @property
+    def first_selector(self):
+        name = iter(self.selectors)
+        c = next(name)
+        return self.selectors[c]
 
-    def hide_selector(self, name):
-        self.get_selector(name).pack_forget()
+    def get_next_selector(self, name=None):
+        """Returns the selector that is located immediately below the one specified
+        Return None if there isn't any"""
+        temp_list = list(self.selectors)
+        if not name:
+            return self.selectors[temp_list[0]]
+        index = temp_list.index(name)+1
+        if index < len(temp_list):
+            return self.selectors[temp_list[index]]
+        return None
 
-    def unhide_selector(self, name):
+    def disable_next_selectors(self, name):
+        """Disable all selector objects located below the current one"""
         temp_list = list(self.selectors)
         temp_list = temp_list[temp_list.index(name)+1:]
-        for id in temp_list:
-            self.hide_selector(id)
-        self.get_selector(name).pack(self.padding)
-        for id in temp_list:
-            self.get_selector(id).pack(self.padding)
+        for i in temp_list:
+            self.get_selector(i).set_state('disabled')
+
+    def get_selector_options(self):
+        temp_dict = {}
+        for selector in self.selectors.values():
+            temp_dict[selector.label] = selector.val
+        return temp_dict
 
     def create_button(self, name):
         button = tk.Button(self, text=name)
@@ -131,6 +166,22 @@ class SidePanel(tk.Frame):
 
     def bind_button(self, name, command):
         self.buttons[name].bind('<Button>', command)
+
+    def bind_selectors(self, func, add=None):
+        for selector in self.selectors.values():
+            selector.bind(func, add)
+
+    def hide_selector(self, name):
+        self.get_selector(name).pack_forget()
+
+    def unhide_selector(self, name):
+        temp_list = list(self.selectors)
+        temp_list = temp_list[temp_list.index(name)+1:]
+        for i in temp_list:
+            self.hide_selector(i)
+        self.get_selector(name).pack(self.padding)
+        for i in temp_list:
+            self.get_selector(i).pack(self.padding)
 
 class Selector(tk.Frame):
     def __init__(self, master=None, cnf={}, **kwargs):
@@ -146,6 +197,18 @@ class Selector(tk.Frame):
         label.pack(pack)
         self.__combobox.pack(pack)
 
+    def set_state(self, state):
+        self.__combobox['state'] = state
+        if state == 'disabled':
+            self.__combobox_var.set('')
+            self.__combobox['values'] = []
+
+    def bind(self, func, add=None):
+        """Binds the combobox"""
+        def bind_function(event):
+            func(self.__label_var.get())
+        self.__combobox.bind('<<ComboboxSelected>>', bind_function, add)
+
     @property
     def label(self) -> None:
         return self.__label_var.get()
@@ -155,16 +218,18 @@ class Selector(tk.Frame):
         self.__label_var.set(text)
 
     @property
-    def combobox_val(self):
+    def val(self):
         return self.__combobox_var.get()
 
-    @combobox_val.setter
-    def combobox_val(self, val:list) -> None:
+    @val.setter
+    def val(self, val:list) -> None:
         self.__combobox['values'] = val
-
-    def set_state(self, state):
-        self.__combobox['state'] = state
+        self.__combobox['state'] = 'readonly'
+        self.__combobox_var.set('')
 
 if __name__ == '__main__':
-    a = TabManager()
-    a.run()
+    root = tk.Tk()
+    a = GraphFactory.get_instance('Corr', root)
+    b = GraphFactory.get_instance('Corr', root)
+    print(id(a))
+    print(id(b))
