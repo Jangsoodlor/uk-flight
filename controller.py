b425f41f157d0323c1a588dfb20213d5ee894abf
Rattapoom Pethputong <celica2548@gmail.com>
2024-05-01 20:54:04 +0700

    add a method to handle plotting graph

--- controller.py
+++ controller.py
@@ -11,6 +11,8 @@ class Controller:
         self.view.run()
 
     def feed_init_data(self):
+        """filled the first selector of each graph with initial datas.
+        And bind the plot button."""
         graphs = self.view.get_all_graphs()
         for graph in graphs:
             first_box_name = graph.side_panel.first_selector
@@ -18,10 +20,23 @@ class Controller:
             first_box_name.val = data
             graph.side_panel.disable_next_selectors(first_box_name.label)
             graph.side_panel.bind_selectors(self.selector_selected)
+            self.bind_plot_buttons(graph)
+            
+
+    def bind_plot_buttons(self, graph):
+        graph.side_panel.bind_button('PLOT', self.activate_plot)
+
+    def activate_plot(self, event):
+        graph = self.view.get_current_graph()
+        graph_name = self.view.get_current_tab_name()
+        options = graph.side_panel.get_selector_options()
+        data = self.model.get_graph_data(graph_name, options)
+        graph.plot_graph(data)
+        
 
     def selector_selected(self, selector_name):
-        current_tab = self.view.graph_dict[self.view.get_current_tab()]
-        current_graph = self.view.graph_factory.get_instance(current_tab)
+        """Fill the next selector with data after the first one is selected"""
+        current_graph = self.view.get_current_graph()
         next_selector = current_graph.side_panel.get_next_selector(selector_name)
         if next_selector:
             current_graph.side_panel.disable_next_selectors(selector_name)
