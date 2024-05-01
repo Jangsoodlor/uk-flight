15412e6b730927c20c29c4430deb12244b0a56c9
Rattapoom Pethputong <celica2548@gmail.com>
2024-05-01 17:08:56 +0700

    add code to get value for comboboxes based on filters

--- model.py
+++ model.py
@@ -40,7 +40,7 @@ class Model:
         temp_df = self.__df[self.__df['airline_name'] == airline]
         temp_df = self.filter_origin_destination(temp_df, origin, destination)
         temp_df = temp_df.loc[:, ['average_delay_mins', 'previous_year_month_average_delay']]
-        return temp_df.corr()
+        return temp_df
 
     def bar_graph_data(self, airlines, origin:str=None, destination:str=None):
         """Returns data for bar graph"""
@@ -54,15 +54,25 @@ class Model:
         temp_df = self.filter_origin_destination(temp_df, origin, destination)
         return temp_df.loc[:, ['number_flights_matched', 'number_flights_cancelled']]
 
-    def get_airport_name(self):
-        return list(self.__df['reporting_airport'].unique())
-
+    def get_selector_data(self, name:str, filter:dict=None):
+        translate = {'Airline':'airline_name',
+                          'Origin (Optional)' : 'reporting_airport',
+                          'Origin' : 'reporting_airport',
+                          'Destination (Optional)' : 'origin_destination',
+                          'Destination' : 'origin_destination'}
+        temp_df = self.__df.copy()
+        if filter:
+            for key, val in filter.items():
+                column = translate[key]
+                if val:
+                    temp_df = temp_df[temp_df[column] == val]
+        return list(temp_df[translate[name]].unique())
 
 if __name__ == '__main__':
     df = pd.read_csv(os.path.join(os.getcwd(),
                                   'data/202401_Punctuality_Statistics_Full_Analysis.csv'))
 
     df_model = Model(df)
-    # print(df_model.corr_data('LOGANAIR LTD'))
+    print(df_model.corr_data('LOGANAIR LTD'))
     # print(df_model.bar_graph_data(['LOGANAIR LTD', 'EASTERN AIRWAYS']))
-    print(df_model.get_airport_name())
+    # print(df_model.get_origins())
