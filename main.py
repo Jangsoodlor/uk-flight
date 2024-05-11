5d41dbb0dea157929fb398c406faa954357027a7
Rattapoom Pethputong <celica2548@gmail.com>
2024-05-11 12:02:35 +0700

    rename data to dataset to avoid confusion with screenshots/data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from controller import Controller
 from model import Model
 from view import TabManager
 df = pd.read_csv(os.path.join(os.getcwd(),
-                                'data/202401_Punctuality_Statistics_Full_Analysis.csv'))
+                                'dataset/202401_Punctuality_Statistics_Full_Analysis.csv'))
 m = Model(df)
 v = TabManager()
 c = Controller(v,m)
