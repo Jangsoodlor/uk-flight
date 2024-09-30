c68e47adc7e0567a8fe101b028e10f324ed47213
Rattapoom Pethputong <celica2548@gmail.com>
2024-09-30 20:14:18 +0700

    add ruff.toml

--- descstat.py
+++ descstat.py
@@ -1,10 +1,16 @@
 """A UI for Descriptive Statistic Tab"""
+
 import tkinter as tk
 from tkinter import ttk
 
 
 class DescStat(tk.Frame):
-    """A descriptive Statistics Tab."""
+    """Return something
+
+    :param tk: _description_
+    :type tk: _type_
+    """
+
     def __init__(self, master=None, cnf={}, **kwargs):
         super().__init__(master, cnf, **kwargs)
         self.__combobox_var = tk.StringVar()
@@ -14,30 +20,31 @@ class DescStat(tk.Frame):
     def init_components(self):
         """Initialise components"""
         top_frame = tk.Frame(self)
-        label = tk.Label(top_frame, text='Select Airport')
-        self.__combobox = ttk.Combobox(top_frame,
-                                       textvariable=self.__combobox_var)
-        self.__combobox['state'] = 'readonly'
-        label.pack(side='left')
-        self.__combobox.pack(side='right')
-        top_frame.pack(side='top', anchor=tk.W, padx=10, pady=10)
+        label = tk.Label(top_frame, text="Select Airport")
+        self.__combobox = ttk.Combobox(top_frame, textvariable=self.__combobox_var)
+        self.__combobox["state"] = "readonly"
+        label.pack(side="left")
+        self.__combobox.pack(side="right")
+        top_frame.pack(side="top", anchor=tk.W, padx=10, pady=10)
 
         self.__textbox = tk.Text(self)
-        self.__textbox['state'] = 'disabled'
-        self.__textbox.pack(expand=True, fill='both')
+        self.__textbox["state"] = "disabled"
+        self.__textbox.pack(expand=True, fill="both")
 
     def binder(self, func, add=None):
         """Binds the combobox"""
+
         def bind_function(event):
             func(self.__combobox_var.get())
-        self.__combobox.bind('<<ComboboxSelected>>', bind_function, add)
+
+        self.__combobox.bind("<<ComboboxSelected>>", bind_function, add)
 
     def insert_text(self, text):
         """Insert texts to textbox"""
-        self.__textbox['state'] = 'normal'
+        self.__textbox["state"] = "normal"
         self.__textbox.delete(1.0, tk.END)
         self.__textbox.insert(tk.END, text)
-        self.__textbox['state'] = 'disabled'
+        self.__textbox["state"] = "disabled"
 
     @property
     def val(self):
@@ -47,5 +54,5 @@ class DescStat(tk.Frame):
     @val.setter
     def val(self, val: list) -> None:
         """Set combobox value"""
-        self.__combobox['values'] = val
-        self.__combobox_var.set('')
+        self.__combobox["values"] = val
+        self.__combobox_var.set("")
